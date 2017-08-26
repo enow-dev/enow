38cb937309f661528285663d963854d20cbbc543
kokikono <kk.k.0421@outlook.jp>
2017-08-26 13:06:08 +0900

    update:  template

--- front/src/index.js
+++ front/src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import reducers from './Reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createHistory();
+const middleware = routerMiddleware(history);
+
+const reducer = combineReducers({
+  ...reducers,
+  router: routerReducer,
+});
+
+const store = createStore(reducer, applyMiddleware(middleware));
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <MuiThemeProvider>
+        <App />
+      </MuiThemeProvider>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
