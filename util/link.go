5aeb3dc37a7737d506e59c223c37c5f2d084e4a1
pei0804 <peeeei0804@gmail.com>
2017-08-31 02:22:20 +0900

    add: header link作成関数

---
+++ util/link.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+	"fmt"
+
+	"net/url"
+
+	"github.com/goadesign/goa"
+	"github.com/tomnomnom/linkheader"
+	"google.golang.org/appengine/search"
+)
+
+// CreateLinkHeader レスポンスヘッダのLinkに付加するページングリンク情報を作成する
+func CreateLinkHeader(reqData *goa.RequestData, scheme string, cursor search.Cursor) linkheader.Link {
+	if cursor == "" {
+		return linkheader.Link{}
+	}
+	uURI, err := url.Parse(reqData.RequestURI)
+	if err != nil {
+		return linkheader.Link{}
+	}
+	u := fmt.Sprint(scheme, reqData.Host, uURI.Path, "?cursor=", cursor)
+	return linkheader.Link{URL: u, Rel: "next"}
+}
