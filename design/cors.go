2c8f207da96088c9ffd58f17ffc1c39b95eec922
pei0804 <peeeei0804@gmail.com>
2017-09-01 22:32:54 +0900

    fix: corsで足りない設定を追加

--- design/cors.go
+++ design/cors.go
@@ -5,23 +5,23 @@ import "github.com/deadcheat/goacors"
 var devCORS = &goacors.GoaCORSConfig{
 	AllowOrigins:     []string{"*"},
 	AllowMethods:     []string{goacors.GET, goacors.HEAD, goacors.PUT, goacors.POST, goacors.DELETE, goacors.OPTIONS},
-	AllowHeaders:     []string{"Content-Type", "X-Authorization"},
+	AllowHeaders:     []string{"Content-Type", "X-Authorization", "Link", "X-Search-Hits-Count"},
 	AllowCredentials: false,
 	MaxAge:           600,
 }
 
 var stagingCORS = &goacors.GoaCORSConfig{
-	AllowOrigins:     []string{"https://enow-staging.appspot.com"},
+	AllowOrigins:     []string{"https://staging-logbook.dojo-voyage.net"},
 	AllowMethods:     []string{goacors.GET, goacors.HEAD, goacors.PUT, goacors.POST, goacors.DELETE, goacors.OPTIONS},
-	AllowHeaders:     []string{"Content-Type", "X-Authorization"},
+	AllowHeaders:     []string{"Content-Type", "X-Authorization", "Link", "X-Search-Hits-Count"},
 	AllowCredentials: false,
 	MaxAge:           600,
 }
 
 var productionCORS = &goacors.GoaCORSConfig{
-	AllowOrigins:     []string{"https://enow.jp"},
+	AllowOrigins:     []string{"https://logbook.dojo-voyage.net"},
 	AllowMethods:     []string{goacors.GET, goacors.HEAD, goacors.PUT, goacors.POST, goacors.DELETE, goacors.OPTIONS},
-	AllowHeaders:     []string{"Content-Type", "X-Authorization"},
+	AllowHeaders:     []string{"Content-Type", "X-Authorization", "Link", "X-Search-Hits-Count"},
 	AllowCredentials: false,
 	MaxAge:           600,
 }
