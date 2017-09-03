14ecfe54586756aa4630fb87cab6187bd4d64fdb
kokikono <kk.k.0421@outlook.jp>
2017-09-03 16:53:33 +0900

    fix: 404ページ作成

--- front/src/Components/NotFound.js
+++ front/src/Components/NotFound.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function NotFounf(props) {
-  const CLIENT_ID = "87a42765a18adc939d0a";
-  console.log('start aouth');
-  window.location.href = "https://github.com/login/oauth/authorize?client_id="+CLIENT_ID+"&scope=user:email&redirect_uri=http://localhost:3000/login";
+import { withStyles } from 'material-ui/styles';
+import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+
+import SearchBox from './SearchBox';
+
+const styles = {
+  container: {
+    width: '100%',
+    height: '50%',
+    margin: 0,
+    padding: 10,
+  },
+  item: {
+    width: '80%',
+    height: '50%',
+  },
+  message: {
+    padding: 20,
+  },
+};
+
+function NotFound({ classes, history }) {
+  return (
+    <Grid
+      container
+      className={classes.container}
+      align="center"
+      direction="column"
+      justify="center"
+    >
+      <Grid item className={classes.item}>
+        <Paper elevation={4}>
+          <div className={classes.message}>
+            <Typography gutterBottom type="display1">お探しのページは見つかりませんでした。</Typography>
+            <Typography type="headline">他のイベントを探してみませんか？</Typography>
+          </div>
+          <SearchBox handleSubmit={() => { history.push('/events'); }} />
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 }
+
+NotFound.propTypes = {
+  classes: PropTypes.object.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+NotFound.defaultProps = {
+  classes: Object,
+  history: PropTypes.shape({
+    push: () => {},
+  }).isRequired,
+};
+
+export default withStyles(styles)(NotFound);
