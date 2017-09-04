5fa5e71fa6b9d9ae180cbcec87a1028632ad31f7
kokikono <kk.k.0421@outlook.jp>
2017-09-04 23:05:30 +0900

    fix: 検索状態をCookieに保持

--- front/src/Actions/SearchStash.js
+++ front/src/Actions/SearchStash.js
@@ -1,9 +1,29 @@
+import Cookies from 'universal-cookie';
+
 import * as types from '../Constants/ActionTypes';
 
-export const clearSearchStash = () => (
-  { type: types.CLEAR_SEARCH }
-);
+const cookies = new Cookies();
+const ST_KEY = 'search_stash';
 
-export const setSearchStash = search => (
-  { type: types.SET_SEARCH, search }
-);
+export const clearSearchStash = (dispatch) => {
+  cookies.remove(ST_KEY, { path: '/' });
+  return dispatch({ type: types.CLEAR_SEARCH });
+};
+
+export function setSearchStash(search) {
+  cookies.remove(ST_KEY, { path: '/' });
+  const expireDate = new Date();
+  expireDate.setDate(expireDate + 1);
+  cookies.set(ST_KEY, search, { path: '/', expire: expireDate });
+  return dispatch => (dispatch({ type: types.SET_SEARCH, search }));
+}
+
+export function isCookieST() {
+  return (dispatch) => {
+    const cookieST = cookies.get(ST_KEY);
+    if (cookieST === undefined) {
+      return Promise.resolve();
+    }
+    return dispatch(setSearchStash(cookieST));
+  };
+}
