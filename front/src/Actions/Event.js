2cee7025ca9da88ebae663f1eca5fd11b5304ee2
kokikono <kk.k.0421@outlook.jp>
2017-09-20 13:04:21 +0900

    fix: fetch -> axios events/{eventID}

--- front/src/Actions/Event.js
+++ front/src/Actions/Event.js
@@ -1,33 +1,24 @@
-import Cookies from 'universal-cookie';
 import * as types from '../Constants/ActionTypes';
+import MyAexios from '../Constants/MyAexios';
 
-const cookies = new Cookies();
 export const receiveEvent = event => (
   { type: types.RECEIVE_EVENT, event }
 );
 export const fetchEvent = () => ({ type: types.FETCH_EVENT });
 
 function getEvent(eventId) {
-  const { REACT_APP_API_Scheme, REACT_APP_API_Host } = process.env;
-  const url = `${REACT_APP_API_Scheme}${REACT_APP_API_Host}/api/events/${eventId}`;// eslint-disable-line
   return (dispatch) => {
     dispatch(fetchEvent());
-    const aouth = cookies.get('aouth');
-    return fetch(url, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/vnd.event+json', // eslint-disable-line
-        'X-Authorization': `${aouth.token}`,
-        'Content-Type': 'application/json',
-      },
+    MyAexios.get(`events/${eventId}`, {
+      mode: 'cors',
+      responseType: 'json',
     })
       .then((response) => {
         if (response.status !== 200) {
           dispatch({ type: types.ADD_ERROR, error: response });
         }
-        return response.json();
-      })
-      .then(responseJson => dispatch(receiveEvent({ item: responseJson })));
+        dispatch(receiveEvent({ item: response.data }));
+      });
   };
 }
 
